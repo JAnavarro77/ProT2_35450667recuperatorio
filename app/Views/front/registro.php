a35641637dbe45b101f36e84d3893def244f6fd7
JAnavarro77 <navarrojuanalberto@gmail.com>
2024-07-09 18:58:19 -0300

    creacion de pagina enlazada con routeo

---
+++ app/Views/front/registro.php
@@ -0,0 +1,12 @@
+<section>
+<h2>Contacto</h2>
+<h3>Puedes ponerte en contacto conmigo a través del siguiente formulario:</h3>
+<form>
+<p><input type="text" placeholder="Nombre"></p>
+<p><input type="text" placeholder="Apellido"></p>
+<p> <input type="email" placeholder="Correo electrónico"></p>
+<p><textarea placeholder="Mensaje"></textarea></p>
+<p><input type="submit" value="Enviar">
+<input type=“reset" value=“Borrar"></p>
+</form>
+</section>
