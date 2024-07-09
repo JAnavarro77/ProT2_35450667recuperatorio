06e3f371de1f4ad4edd5afa83f33d846f2b2e32e
JAnavarro77 <navarrojuanalberto@gmail.com>
2024-07-09 19:31:58 -0300

    se agregaron los links de las redes en el footers

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -3,11 +3,11 @@
     <div class="footer-content">
         <div class="social-icons">
             <ul>
-            <a href="#" class="social-icon"><img src="assets/img/face.png" alt="facebook"alt="marca"
+            <a href="https://www.facebook.com/" class="social-icon"><img src="assets/img/face.png" alt="facebook"alt="marca"
             width="75" height="30"></a>
-            <a href="#" class="social-icon"><img src="assets/img/twitter.png" alt="twitter"alt="marca"
+            <a href="https://x.com/" class="social-icon"><img src="assets/img/twitter.png" alt="twitter"alt="marca"
             width="75" height="30"></a>
-            <a href="#" class="social-icon"><img src="assets/img/insta.png" alt="instagram"alt="marca"
+            <a href="https://www.instagram.com/" class="social-icon"><img src="assets/img/insta.png" alt="instagram"alt="marca"
             width="75" height="30"></a>
 </ul>
 </div>
